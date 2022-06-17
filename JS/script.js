29d10e763da84bf9f3f3f146c16557abaf9be5ed
Jacks113 <59602950+Jacks113@users.noreply.github.com>
2022-06-17 10:56:22 +0200

    desktop design done, needs js optimised

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+// window.onload = function(e){
+//     const navFeatures = document.getElementById("features");
+//     const navCompany = document.getElementById("company");
+//     const hiddenCompany = document.getElementById("hiddenCompany");
+//     const hiddenFeatures = document.getElementById("hiddenFeatures");
+
+
+//     navFeatures.addEventListener("mouseenter", function(){
+//         hiddenFeatures.classList.remove("hide");
+//         hiddenFeatures.classList.add("showFlex");
+//     })
+
+//     hiddenFeatures.addEventListener("mouseleave", function(){
+//         hiddenFeatures.classList.add("hide");
+//         hiddenFeatures.classList.remove("showFlex");
+//     })
+    
+    
+//     navCompany.addEventListener("mouseenter", function(){
+//         hiddenCompany.classList.remove("hide");
+//         hiddenCompany.classList.add("showFlex");
+//     })
+
+//     hiddenCompany.addEventListener("mouseleave", function(){
+//         hiddenCompany.classList.add("hide");
+//         hiddenCompany.classList.remove("showFlex");
+//     })
+
+// }
