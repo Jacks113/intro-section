e4dac70bad1dfa83bf334af9ca6d7722cee66645
Jacks113 <59602950+Jacks113@users.noreply.github.com>
2022-06-21 13:07:13 +0200

    responsive under 1000px

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,24 @@
-// window.onload = function(e){
-//     const navFeatures = document.getElementById("features");
-//     const navCompany = document.getElementById("company");
-//     const hiddenCompany = document.getElementById("hiddenCompany");
-//     const hiddenFeatures = document.getElementById("hiddenFeatures");
+window.onload = function(e){
+    const navFeatures = document.getElementById("features");
+    const navCompany = document.getElementById("company");
+    const hiddenCompany = document.getElementById("hiddenCompany");
+    const hiddenFeatures = document.getElementById("hiddenFeatures");
 
 
-//     navFeatures.addEventListener("mouseenter", function(){
-//         hiddenFeatures.classList.remove("hide");
-//         hiddenFeatures.classList.add("showFlex");
-//     })
+    navFeatures.addEventListener("click", function(){
+        hiddenFeatures.classList.toggle("hide");
+        hiddenFeatures.classList.toggle("showFlex");
 
-//     hiddenFeatures.addEventListener("mouseleave", function(){
-//         hiddenFeatures.classList.add("hide");
-//         hiddenFeatures.classList.remove("showFlex");
-//     })
-    
-    
-//     navCompany.addEventListener("mouseenter", function(){
-//         hiddenCompany.classList.remove("hide");
-//         hiddenCompany.classList.add("showFlex");
-//     })
+        hiddenCompany.classList.add("hide");
+        hiddenCompany.classList.remove("showFlex");
+    })
 
-//     hiddenCompany.addEventListener("mouseleave", function(){
-//         hiddenCompany.classList.add("hide");
-//         hiddenCompany.classList.remove("showFlex");
-//     })
+    navCompany.addEventListener("click", function(){
 
-// }
+        hiddenFeatures.classList.add("hide");
+        hiddenFeatures.classList.remove("showFlex");
+
+        hiddenCompany.classList.toggle("hide");
+        hiddenCompany.classList.toggle("showFlex");
+    })
+}
