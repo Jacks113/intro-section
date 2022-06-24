1af7c4239322aa44d3f8506d9fe51bbee9d7b981
Jacks113 <59602950+Jacks113@users.noreply.github.com>
2022-06-24 13:16:39 +0200

    event listener desktop features company

--- JS/script.js
+++ JS/script.js
@@ -6,50 +6,79 @@ window.onload = function(e){
 
     const navCompany = document.getElementById("company");
 
-    window.addEventListener("resize", function(){
 
-        if (window.matchMedia("(min-width: 700px)").matches){
-                console.log("Win size 700px");
-        }
-       
-    })
+     // functions for adding and removing hover for features
 
-
-    navFeatures.addEventListener("mouseover", function(){
+     const addHoverNavFeat = function(){
         document.getElementById("features-arrow").src="/images/icon-arrow-up.svg";
+
         document.getElementsByClassName("basics")[0].classList.remove("overflow-hide");
-    })
-    
-    navFeatures.addEventListener("mouseout", function(){
+
+
+        document.getElementById("features-drop").style.display="flex";
+
+       
+    }
+        
+    const removeHoverNavFeat = function(){
         document.getElementById("features-arrow").src="/images/icon-arrow-down.svg";
+
         document.getElementsByClassName("basics")[0].classList.add("overflow-hide");
-    })
-    
-    
-    navCompany.addEventListener("mouseover", function(){
-        document.getElementById("company-arrow").src="/images/icon-arrow-up.svg";
-        document.getElementsByClassName("basics")[0].classList.remove("overflow-hide");
-    })
-    
-    navCompany.addEventListener("mouseout", function(){
-        document.getElementById("company-arrow").src="/images/icon-arrow-down.svg";
-        document.getElementsByClassName("basics")[0].classList.add("overflow-hide");
-    })
+
+        document.getElementById("features-drop").style.display="none";
+
+    }
+
+    // functions for adding and removing hover for company
+        const addHoverCompany = function(){
+            document.getElementById("company-arrow").src="/images/icon-arrow-up.svg";
+
+            document.getElementsByClassName("basics")[0].classList.remove("overflow-hide");
 
 
-    document.getElementById("learn").addEventListener("click", function(){
-        window.location.href='#';
-    })
+            document.getElementById("company-drop").style.display="flex";
 
+            document.getElementById("company-drop").classList.remove("dropdown-none");
+            document.getElementById("company-drop").classList.add("dropdown-show");
+            }
+
+
+        const removeHoverCompany = function(){
+            document.getElementById("company-arrow").src="/images/icon-arrow-down.svg";
+        
+            document.getElementById("company-drop").style.display="none";
+        
+            document.getElementsByClassName("basics")[0].classList.add("overflow-hide");
+        }
+  
+
+
+        //  function for changing event listener based on window size
+        const onResize = function(){
+            console.log(window.innerWidth);
+
+            // removing event listeners for hover on features and company in navigation if under 1000px
+
+            if (window.innerWidth < 1000){
+                navFeatures.removeEventListener("mouseover", addHoverNavFeat);
+                navFeatures.removeEventListener("mouseout", removeHoverNavFeat);
+
+                navCompany.removeEventListener("mouseout", removeHoverCompany);
+                navCompany.removeEventListener("mouseover", addHoverCompany);
+            }
+    }
     
-    document.getElementsByClassName("header-logo")[0].addEventListener("click", function(){
-        window.location.href='';
-    })
-    
-    document.getElementsByClassName("logo")[0].addEventListener("click", function(){
-        window.location.href='';
-    })
-    
+
+    // adding event listeners for hover on features and company in navigation
+    navFeatures.addEventListener("mouseover", addHoverNavFeat);
+    navFeatures.addEventListener("mouseout", removeHoverNavFeat);
+
+    navCompany.addEventListener("mouseout", removeHoverCompany);
+    navCompany.addEventListener("mouseover", addHoverCompany);
+
+    // window resize
+        window.onresize = onResize;
+
 }
 
 function changeNav(){
